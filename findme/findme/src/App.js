ba4abe7ccbb90b8a65231d75e70efc17413c1a25
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-04-27 21:39:49 +0530

    up

--- findme/findme/src/App.js
+++ findme/findme/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
-import {getPosts} from './actions/posts';
+import { getPosts } from './actions/posts';
 import Image from './Images/logo.png';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Forms/Form.js';
@@ -10,30 +10,28 @@ import useStyles from './styles.js';
 function App() {
   const classes = useStyles();
   const dispatch = useDispatch();
-  useEffect (() => {
+  useEffect(() => {
     dispatch(getPosts());
   }, [dispatch])
 
   return (
-    <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit' className={classes.appBar}>
-        <Typography variant='h2' align='center' className={classes.heading}>
-          FindMe
-        </Typography>
-        <img src={Image} height="60"></img>
-        <Grow in>
-          <Container>
-            <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
-              <Grid item xs={12} sm={7}>
-                <Posts />
-                </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form />
-                </Grid>
-            </Grid>
-          </Container>
-        </Grow>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={Image} alt="icon" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
--- findme/findme/src/components/Forms/Form.js
+++ findme/findme/src/components/Forms/Form.js
@@ -29,25 +29,19 @@ const Form = (props) => {
             file: '',
         });
     }
+    const currentId = false
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Create a Post</Typography>
+                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-                <TextField name="tile" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-                <TextField name="content" variant="outlined" label="Content" fullWidth value={postData.content} onChange={(e) => setPostData({ ...postData, content: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
-                <div className={classes.fileInput}>
-                    <FileBase type="file" multiple={false} onDone={(file) => setPostData({ ...postData, file: file.base64 })} />
-                </div>
-                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>
-                    Create Post
-                </Button>
-                <Button className={classes.buttonSubmit} variant="contained" color="secondary" size="small" fullWidth onClick={clear}>
-                    Clear
-                </Button>
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+                <TextField name="content" variant="outlined" label="Content" fullWidth multiline rows={4} value={postData.content} onChange={(e) => setPostData({ ...postData, content: e.target.value })} />
+                <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
-
         </Paper>
     );
 }
