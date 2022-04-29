1381df4164362ad43863a9e6dbb9c7e16366c09f
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-04-29 21:28:53 +0530

    Update posts.js

--- findme/findme/src/actions/posts.js
+++ findme/findme/src/actions/posts.js
@@ -2,10 +2,8 @@ import * as api from '../api/index.js';
 
 export const getPost = (id) => async (dispatch) => {
     try {
-        // dispatch({ type: START_LOADING });
-
+        dispatch({ type: 'START_LOADING' });
         const { data } = await api.fetchPost(id);
-
         dispatch({ type: 'FETCH_POST', payload: { post: data } });
     } catch (error) {
         console.log(error);
@@ -14,8 +12,10 @@ export const getPost = (id) => async (dispatch) => {
 
 export const getPosts = (page) => async (dispatch) => {
     try {
+        dispatch({ type: 'START_LOADING' });
         const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
         dispatch({ type: 'FETCH_ALL', payload: { data, currentPage, numberOfPages } });
+    dispatch({ type: 'END_LOADING' });
     } catch (error) {
         console.log(error);
     }
